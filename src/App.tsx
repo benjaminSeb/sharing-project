9df7ef36a5e3a2e1ef804a038f7558c2b1917723
Benjamin SEBILLE <sebille.benjamin@live.fr>
2020-09-26 13:07:04 +0200

    First commit - TypeScript + Material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
