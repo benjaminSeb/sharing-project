825246dcc3a711185c5ee78469a878cf9c9852f5
Benjamin SEBILLE <sebille.benjamin@live.fr>
2020-11-11 13:10:48 +0100

    Add some element - need use material:makeStyles and useStyles to change Paper background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import LongMenu from './Menu';
+import { Button, Grid, Icon, TextField, AppBar, Toolbar, IconButton, Typography, InputBase, Paper, makeStyles } from '@material-ui/core';
+import ChildCareIcon from '@material-ui/icons/ChildCare';
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+import classes from '*.module.css';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: theme.spacing(16),
+    height: theme.spacing(16),
+    backgroundColor: "blueviolet"
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid className="App-grid" container direction="column" alignItems="center" justify="space-between">
+        <Grid className="App-header-grid" container direction="row" justify="center">
+          {/* <AppBar position="static"> */}
+            <Toolbar>
+              <IconButton edge="start" className="Toolbar-menu-button" color="primary" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className="Toolbar-site-title">
+                SiteName
+              </Typography>
+              <div className="Toolbar-search">
+                <SearchIcon />
+                <InputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
+              <Button variant="contained" color="primary">Se Connecter</Button>
+            </Toolbar>
+        </Grid>
+        <Grid className="App-center-grid" container direction="row" justify="flex-end">
+          <Button className="App-center-about" variant="text">About</Button>
+        </Grid>
+        <Grid className="App-bottom-grid" container direction="row" justify="center">
+          <Paper className={classes.paper} />
+          <Paper className="bottom-paper" />
+          <Paper className="bottom-paper" />
+        </Grid>
+      </Grid>
     </div>
   );
 }
